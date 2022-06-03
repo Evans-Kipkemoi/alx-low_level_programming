9e8efc84d722913e5612df44b238a777d96dc87c
Evans-Kipkemoi <evanskipkemoi7@gmail.com>
2022-06-02 19:17:29 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,10 @@
 
 /**
  * main - entry point
- * Description: prints exactly and that piece of art is useful"
- * -Dora Korpar, 2015-10-19"
- *  Return: Always 0 Success
+ * Description: prints exactly "and that piece of art is useful"
+ * -Dora Korpar, 2015-10-19,
+ *  followed by a new line, to the standard error.
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
