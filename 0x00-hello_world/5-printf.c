61af3f4bdc03f947a18589122e347090ebee9755
Evans-Kipkemoi <evanskipkemoi7@gmail.com>
2022-06-02 17:58:34 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - prints the phrase with the proper grammar,
  * but the outcome is a piece of art,
- * return: 0 if exited well, otherwise non-zero
+ * Return: 0 if exited well, otherwise non-zero
  */
 int main(void)
 {
