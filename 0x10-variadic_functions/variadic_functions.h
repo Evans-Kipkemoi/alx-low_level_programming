7d7917a13c98d7fe3e93e3d5eb7e35e1092a5db2
Evans-Kipkemoi <evanskipkemoi7@gmail.com>
2022-07-07 16:19:33 -0700

    Function that prints strings

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,17 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/*
+ * struct check - paramenters to check against
+ * @c: type
+ * @f: the function to send it to
+ */
+typedef struct check
+{
+		char *chk;
+			void (*f)(va_list list);
+} check_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
